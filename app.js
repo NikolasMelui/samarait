984b02a8b02e9aa0cb6fad406f41dcf9268b32d4
NikolasMelui <slak@samaradom.ru>
2017-10-02 23:56:48 +0400

    Ooops))

--- app.js
+++ app.js
@@ -36,13 +36,13 @@ const app = express();
   app.use('/', index);
 
   // Use sessionStore for saving client session
-  const sessionStore = require('./libs/sessionStore');
-  app.use(session({
-    secret: myconfig.session.secret,
-    key: myconfig.session.key,
-    cookie: myconfig.session.cookie,
-    store: sessionStore
-  }));
+  // const sessionStore = require('./libs/sessionStore');
+  // app.use(session({
+  //   secret: myconfig.session.secret,
+  //   key: myconfig.session.key,
+  //   cookie: myconfig.session.cookie,
+  //   store: sessionStore
+  // }));
 
   /*
   I think here must be some custome commonMiddleware,
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Samata IT Community' });
 });
 
 module.exports = router;
